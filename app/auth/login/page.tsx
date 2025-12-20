fa97616ef6b4a0a616cbcaff82dfc301b28f9f53
Dev Raj Dev <thakurdev2672@gmail.com>
2025-12-21 00:10:36 +0530

    setup for strimo app with nextjs

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="bg-red-500  ">
+           <h1 className="mx-auto">hiii this is a login Page</h1>
+        </div>
+    );
+}
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,7 @@
+const register=()=>{
+    return (
+        <div>Hii this is a register page</div>
+    )
+
+}
+export default register
