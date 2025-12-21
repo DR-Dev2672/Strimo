49dcdb4cb62510698744e3e5032caa11abc83f0f
Dev Raj Dev <thakurdev2672@gmail.com>
2025-12-21 17:15:23 +0530

    middleware for clerk and signIn and Userbutton added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
+      <SignInButton/>
       Strimo clone setup done
+      <UserButton/>
     </div>
     
   );
