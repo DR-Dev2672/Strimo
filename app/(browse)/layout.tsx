432a11747e074348aaf03609105f11f07063ac63
Dev Raj Dev <thakurdev2672@gmail.com>
2026-01-05 05:51:13 +0530

    flow of layout.tsx added for browse

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,12 +1,24 @@
 import { UserButton } from "@clerk/nextjs";
 import Navbar from "./_components/Navbar";
+import { Suspense } from "react";
+import SideBarSkeleton from "./_components/sidebarskeleton";
+import {Sidebar} from "./_components/sidebar";
+import { Container } from "./_components/container";
 
 export default function BrowseLayout({children}: {children: React.ReactNode}) {
     return (
-        <div>
+        <>
              <Navbar />
+             <div className="flex h-full pt-20 ">
+             <Suspense fallback={<SideBarSkeleton/>}>
+              <Sidebar/>
+             </Suspense>
+             
              {/* <UserButton/> */}
+             <Container>
             {children}
-        </div>
+            </Container>
+            </div>
+        </>
     );
 }
