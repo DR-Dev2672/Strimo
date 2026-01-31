1ebf50c40ed667de5d77ed87ba56777ac01fe182
Dev Raj Dev <thakurdev2672@gmail.com>
2026-01-31 21:35:54 +0530

    db connection check added

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"], // optional
-  });
+export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// async function checkDbConnection() {
+//   try {
+//     await db.$queryRaw`SELECT 1`;
+//     console.log("✅ Database connected successfully");
+//   } catch (error) {
+//     console.error("❌ Database connection failed:", error);
+//   }
+// }
+
+// // Run check immediately
+// checkDbConnection();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
