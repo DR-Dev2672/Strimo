ec641e64a6e196f83bbe73045d22dd8b1255e077
Dev Raj Dev <thakurdev2672@gmail.com>
2026-02-10 23:09:35 +0530

    recommended ui added

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,15 +1,38 @@
 "use client"
+import {User} from "@prisma/client";
 import { useSidebar } from "@/store/use-sidebar"
+import { UserItem } from "./user-item";
 
-export  function Recommended() {
+interface RecommendedProps{
+    data: User[];
+}
+
+export  function Recommended({data}:RecommendedProps ) {
     const {collapsed}=useSidebar((state)=>state);
+    const showLabel= (!collapsed) && (data.length) >0;
+
     return (
-        <>{
-            !collapsed && 
+        <>
+            {showLabel && (
            <div className="pl-6 mb-4">
                 <p className="text-sm text-muted-foreground">Recommended</p>
-                </div>
-        }
+                </div>)}
+
+            <ul className="space-y-2 px-2">
+                {
+                    data.map((user)=>(
+                        
+                        <UserItem 
+                        key={user.id}
+                        imageUrl={user.imageUrl}
+                        username={user.username}
+                        
+
+                        />
+                    ))
+                }
+            </ul>
+        
         
         </>
     );
