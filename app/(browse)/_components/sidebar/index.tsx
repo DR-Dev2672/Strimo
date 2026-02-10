6e0750c0a7f673a9c97dc4add984fa0290a3272c
Dev Raj Dev <thakurdev2672@gmail.com>
2026-02-10 23:11:05 +0530

    index file updated sidebar

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,15 +1,17 @@
+import { getRecommended } from "@/lib/recommended-service";
 import { Following } from "./following";
 import { Recommended } from "./recommended";
 import { Toggle } from "./toggle";
 import { Wrapper } from "./wrapper";
 
-export  function Sidebar() {
+export  const  Sidebar = async()=> {
+    const recommended= await getRecommended();
     return (
        <Wrapper>
             <Toggle/>
             <div className="space-y-4 pt-4 lg:pt-0">
              <Following/>
-             <Recommended/>
+             <Recommended data={recommended}/>
              </div>
         </Wrapper>
     );
