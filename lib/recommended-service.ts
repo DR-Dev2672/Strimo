1698181d6ac85f1ea1397a8b0f43f40685c74992
Dev Raj Dev <thakurdev2672@gmail.com>
2026-02-10 23:07:41 +0530

    recommended service added

---
+++ lib/recommended-service.ts
@@ -0,0 +1,29 @@
+import { getSelf } from "./auth-service";
+import { prisma } from "@/lib/prisma";
+
+
+
+export const getRecommended=async()=>{
+
+
+let selfId;
+try {
+    const self=await getSelf();
+    selfId=self.id;
+} catch  {
+    selfId=null;
+}
+
+let users:any=[]; 
+if(selfId){
+    users=await prisma.user.findMany({
+        where:{
+            id:selfId,
+        }
+    })
+}
+
+
+return users;
+}
+
