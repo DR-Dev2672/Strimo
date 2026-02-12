e25c33be78fa2f8d1db2b41fdaf51630bf2d5eaf
Dev Raj Dev <thakurdev2672@gmail.com>
2026-02-12 20:06:08 +0530

    /username page added

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,5 +1,37 @@
-export default function Page() {
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+import { Action } from "./_components/actions";
+
+
+interface UserPageProps {
+  params: Promise<{
+    username: string;
+  }>;
+}
+
+const UserPage = async ({ params }: UserPageProps) => {
+  const { username } = await params;
+   const user= await getUserByUsername(username);
+
+   if(!user){
+    notFound();
+   }
+     const isFollowing=await isFollowingUser(user.id);
     return (
-        <div>UserPage</div>
+      <>
+        <div className="flex flex-col">
+
+            <p>user:{username}</p>
+            <p>userId: {user.id}</p> 
+            <p>is Following:{` ${isFollowing}`} </p> 
+            
+        </div>
+        <Action isFollowing={isFollowing} userId={user.id} />
+        
+        </>
+      
     );
 }
+
+export default UserPage;
