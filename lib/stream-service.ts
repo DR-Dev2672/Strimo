9feeec7eb88f3780e51864cc37b956d7928b9c37
Dev Raj Dev <thakurdev2672@gmail.com>
2026-02-23 23:46:41 +0530

    stream service call added

---
+++ lib/stream-service.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/lib/prisma";
+
+export const getStreamByUserId = async (userId: string) => {
+  const stream = await prisma.stream.findUnique({
+    where: { userId },
+  });
+
+  return stream;
+};
