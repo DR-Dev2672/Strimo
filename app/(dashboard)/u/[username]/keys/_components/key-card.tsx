e4fb5fbc8a243215f3914eec87997a0855fcbbed
Dev Raj Dev <thakurdev2672@gmail.com>
2026-02-27 23:40:31 +0530

    secret key ui and logic

---
+++ app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { CopyButton } from "./copy-button";
+
+interface KeyCardProps {
+  value: string | null;
+};
+
+export const KeyCard = ({
+  value,
+}: KeyCardProps) => {
+
+    const [show,setShow]=useState(false);
+
+  return (
+    <div className="rounded-xl bg-muted p-6">
+      <div className="flex items-start gap-x-10">
+        <p className="font-semibold shrink-0">
+          Stream Key
+        </p>
+        <div className="space-y-2 w-full">
+          <div className="w-full flex items-center gap-x-2">
+            <Input
+              placeholder="Stream key"
+              disabled
+              type={show?"text":"password"}
+            />
+            <CopyButton />
+          </div>
+          <Button
+          onClick={()=>setShow(!show)}
+          variant="link" 
+          size="sm" 
+          >
+            {show?"Hide":"Show"}
+        
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
