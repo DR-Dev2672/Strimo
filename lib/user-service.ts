06fefd8ab2b4fa192ac481757be32906db10d71b
Dev Raj Dev <thakurdev2672@gmail.com>
2026-03-03 23:54:44 +0530

    user-service updated with additional required properties

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -4,7 +4,30 @@ export const getUserByUsername=async(username:string)=>{
     const user=await prisma.user.findUnique({
         where:{
             username:username,
-        }
+        },
+        select: {
+      id: true,
+      externalUserId: true,
+      username: true,
+      bio: true,
+      imageUrl: true,
+      stream: {
+        select: {
+          id: true,
+          isLive: true,
+          isChatDelayed: true,
+          isChatEnabled: true,
+          isChatFollowersOnly: true,
+          thumbnailUrl: true,
+          name: true,
+        },
+      },
+      _count: {
+        select: {
+          followedBy: true,
+        },
+      },
+    },
     })
     return user;
 }
@@ -13,7 +36,11 @@ export const getUserById=async(id:string)=>{
     const user=await prisma.user.findUnique({
         where:{
             id:id,
-        }
+        },
+        include: {
+      stream: true,
+    },
+
         
     })
     return user;
