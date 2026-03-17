8bf41b132f4baa414ab980e8b056e71b4b3ca2c4
Dev Raj Dev <thakurdev2672@gmail.com>
2026-03-17 23:55:18 +0530

    video comp added

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,7 +1,8 @@
-
+"use client"
 
 import { useViewerToken } from "@/hook/use-viewer-token";
 import { LiveKitRoom } from "@livekit/components-react";
+import { Video } from "./video";
 
 type CustomStream = {
     id: string;
@@ -43,7 +44,12 @@ export const StreamPlayer=(
     return (
         <div>
            <LiveKitRoom serverUrl={process.env.LIVEKIT_URL} token={token} >
-              
+              <div> 
+                <Video
+                hostName={user.username}
+                hostIdentity={user.id}
+                />
+              </div>
            </LiveKitRoom>
         </div>
     )
