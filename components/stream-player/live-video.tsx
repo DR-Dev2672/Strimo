428d96c6f8e9d7ebaf3017cef74c1d25145afd6d
Dev Raj Dev <thakurdev2672@gmail.com>
2026-03-19 23:45:15 +0530

    live video ui added

---
+++ components/stream-player/live-video.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import { useTracks } from "@livekit/components-react";
+import { is } from "date-fns/locale";
+import { Participant, Track } from "livekit-client";
+import { useRef, useState } from "react";
+import { VolumeControl } from "./volume-control";
+import { FullscreenControl } from "./fullscreen-control";
+import { useEventListener } from "usehooks-ts";
+
+interface LiveVideoProps{
+    participant:Participant;
+}
+
+export const LiveVideo=({
+    participant
+}:LiveVideoProps)=>{
+    const videoRef=useRef<HTMLVideoElement>(null);
+    const wrapperRef=useRef<HTMLDivElement>(null);
+
+    useTracks([Track.Source.Camera, Track.Source.Microphone])
+    .filter((track) => track.participant.identity === participant.identity)
+    .forEach((track) => {
+      if (videoRef.current) {
+        track.publication.track?.attach(videoRef.current)
+      }
+    });
+
+    const [volume, setVolume] = useState(0);
+    const [isFullscreen, setIsFullscreen] = useState(false);
+
+    const toggleFullscreen=()=>{  
+      if(isFullscreen){
+        document.exitFullscreen();
+      }
+      else if (wrapperRef?.current){
+        wrapperRef.current.requestFullscreen();
+      }
+    }
+
+    const onVolumeChange = (value: number) => {
+        setVolume(+value); 
+        if(videoRef?.current){
+          videoRef.current.muted=value===0;
+          videoRef.current.volume=(+value)*0.01;
+        }
+    }
+
+    const toggleMute=()=>{
+      const isMuted=volume===0;
+      if(isMuted){
+        setVolume(50);
+      }
+      else{
+        setVolume(0);
+      }
+
+      if(videoRef?.current){
+        videoRef.current.muted=!isMuted;
+        videoRef.current.volume=isMuted?0.5:0;
+      }
+    }
+  //   const handleFullscreenChange = () => {
+  //   const isCurrentlyFullscreen = document.fullscreenElement !== null;
+  //   setIsFullscreen(isCurrentlyFullscreen);
+  // }
+
+  // useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
+
+
+
+    return (
+      <div
+       ref={wrapperRef}
+       className="relative h-full flex bg-red-400">
+        <video
+        ref={videoRef}
+        width="100%"
+        />
+        <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all ">
+         <div className="absolute bottom-0 h-14 w-full flex items-center justify-between bg-gradient-to-r  from-neutral-900 px-4  ">
+           <VolumeControl
+           onChange={onVolumeChange}
+           value={volume}
+           onToggle={toggleMute}
+          />
+          <FullscreenControl
+           isFullScreen={isFullscreen}
+           onToggle={toggleFullscreen}
+          />
+        </div>
+        </div>
+      </div>
+    )
+}
