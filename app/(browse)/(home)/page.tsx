9e58340331a33dc99c23671cca3a4860801e6a6e
Dev Raj Dev <thakurdev2672@gmail.com>
2026-04-12 04:08:23 +0530

    production error sdebug

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,10 @@
-import { Results } from "./_components/results";
+import { Results } from "./_components/Results";
+
 
 export default function Page() {
     return (
         <div className="h-full p-8 max-w-screen-2xl mx-auto">
-            <Results/>
+            <Resultss/>
         </div>
         
     );
