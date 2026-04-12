7056398594ed6c13aa8daf5f934895e3f1f0d099
Dev Raj Dev <thakurdev2672@gmail.com>
2026-04-12 18:23:26 +0530

    spelling mistake solved

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,7 +4,7 @@ import { Results } from "./_components/Results";
 export default function Page() {
     return (
         <div className="h-full p-8 max-w-screen-2xl mx-auto">
-            <Resultss/>
+            <Results/>
         </div>
         
     );
