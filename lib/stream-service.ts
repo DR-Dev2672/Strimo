dd29afd129666fb5c860b22ead0cc4d839a5ac03
Dev Raj Dev <thakurdev2672@gmail.com>
2026-04-12 19:11:48 +0530

    fix 4

--- lib/stream-service.ts
+++ lib/stream-service.ts
@@ -4,6 +4,9 @@ export const getStreamByUserId = async (userId: string) => {
   const stream = await prisma.stream.findUnique({
     where: { userId },
   });
+  if(stream==null){
+    throw new Error("Stream not found");
+  }
 
   return stream;
 };
