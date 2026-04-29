04c958b66c9b0cfac8bc710ff05ed568ee198322
Dev Raj Dev <thakurdev2672@gmail.com>
2026-04-29 14:59:47 +0530

    sidebar hidden when mobile device issue fixed with hint functionality

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,34 +1,53 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useSidebar } from "@/store/use-sidebar";
-import { ArrowRightFromLine } from 'lucide-react';
-import { ArrowLeftFromLine } from 'lucide-react';
-export  function Toggle() {
-     const {collapsed,onExpand,onCollapse}=useSidebar((state)=>state);
-    return (  
-        <>
-        {collapsed && <div className=" lg:flex w-full items-center justify-center pt-4 mb-4">
-           <Button
-            onClick={onExpand} >
-            <ArrowRightFromLine />
-             </Button>
-        </div>}
-        {!collapsed &&<div className="p-3 pl-6 mb-2 flex items-center w-full">
-           <Button
-           onClick={onCollapse} >
-            <ArrowLeftFromLine />
-             </Button>
-        </div>}
-        
-        </>
-    );
-}
+"use client";
 
-export  const ToggleSkeleton=()=>{
-    return (
-        <div>
-            <Skeleton className="h-4 w-[100]"/>
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const Toggle = () => {
+  const {
+    collapsed,
+    onExpand,
+    onCollapse
+  } = useSidebar((state) => state);
+
+  const label = collapsed ? "Expand" : "Collapse";
+
+  return (
+    <>
+      {collapsed && (
+        <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4">
+          <Hint label={label} side="right" asChild>
+            <Button
+              onClick={onExpand}
+              variant="ghost" 
+              className="h-auto p-2"
+            >
+              <ArrowRightFromLine className="h-4 w-4" />
+            </Button>
+          </Hint>
         </div>
-    )
-}
+      )}
+      {!collapsed && (
+        <div className="p-3 pl-6 mb-2 flex items-center w-full">
+          <p className="font-semibold text-primary">
+            For you
+          </p>
+          <Hint label={label} side="right" asChild>
+            <Button
+              onClick={onCollapse}
+              className="h-auto p-2 ml-auto" 
+              variant="ghost"
+            >
+              <ArrowLeftFromLine className="h-4 w-4" />
+            </Button>
+          </Hint>
+        </div>
+      )}
+    </>
+  );
+};
+
