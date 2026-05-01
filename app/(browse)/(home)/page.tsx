ce6654c7c43c163405d233da4d61c7b370c91c33
Dev Raj Dev <thakurdev2672@gmail.com>
2026-05-01 17:47:09 +0530

    new stream feature added

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,37 @@
+import { Button } from "@/components/ui/button";
 import { Results } from "./_components/Results";
+import Link from "next/link";
+import { get } from "http";
+import { currentUser } from "@clerk/nextjs/server";
 
 
-export default function Page() {
+export default async function Page () {
+    const user= await currentUser()
     return (
-        <div className="h-full p-8 max-w-screen-2xl mx-auto">
+        <div className=" h-full p-8 max-w-screen-2xl mx-auto   ">
+
+            { user && (<div className=" flex flex-col justify-content items-center m-4 ">
+              <Link href={`/u/${user?.username}/keys`}>
+                        <Button className="font-bold text-2xl bg-blue-500 rounded-3xl text-black hover:bg-blue-300"
+                        variant="ghost">
+                            Create new stream
+                        </Button>
+            </Link>
+
+            </div>)
+            }
+            { !user && (<div className=" flex flex-col justify-content items-center m-4 ">
+              <Link href={`/sign-in`}>
+                        <Button className="font-bold text-2xl bg-blue-500 rounded-3xl text-black hover:bg-blue-300"
+                        variant="ghost">
+                            Login to create new stream
+                        </Button>
+            </Link>
+
+            </div>)
+            }
+
+        
             <Results/>
         </div>
         
